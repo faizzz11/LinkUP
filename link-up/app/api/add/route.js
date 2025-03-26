faa8db29ff60d5e2e6ecb108ed14b46c23cf8c38
faizzz11 <faizmoulavi54@gmail.com>
2025-03-26 21:42:17 +0530

    added more functionlity to the web

--- link-up/app/api/add/route.js
+++ link-up/app/api/add/route.js
@@ -9,8 +9,15 @@ export async function POST(request) {
     const db = client.db("LinkUP")
     const collection = db.collection("links")
 
+  
+    //agar kisine yeh handle pehle se hi le liya, fir tum yeh handle se linkUP nhi bana skte
+    const doc= await collection.findOne({handle: body.handle})
+    if(doc){
+      return Response.json({ success: false, error: true, message: 'This Handle Already Exists!!',result: null})
+    }
+
     const result=await collection.insertOne(body)
 
-
-    return Response.json({ success: true, error: false,message: 'Links Added',result: result})
+    return Response.json({ success: true, error: false,message: 'Your LinkUP has been Made!!!',result: result})
+    
   }
--- link-up/app/generate/page.js
+++ link-up/app/generate/page.js
@@ -1,80 +1,152 @@
-"use client"
-import React from 'react'
-import { useState } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
+"use client";
+import React from "react";
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const generate = () => {
+//   const [link, setlink] = useState("");
+//   const [linkText, setlinkText] = useState("");
 
-    const [link, setlink] = useState("")
-    const [linkText, setlinkText] = useState("")
-    const [handle, sethandle] = useState("")
-    const [picture, setpicture] = useState("")
-    const addLink = async (text, link, handle) => {
-        const myHeaders = new Headers();
-        myHeaders.append("Content-Type", "application/json");
+const [links, setLinks] = useState([{link:"", linkText:""}])
 
-        const raw = JSON.stringify({
-            "link": link,
-            "link-text": text,
-            "handle": handle
-        });
+const handleChange = (index, link, linkText)=>{
+ setLinks((initialLinks)=>{
+   return initialLinks.map((item,i)=>{
+        if (i ===index){
+            return {link, linkText}
+        }
+        else{
+            return item
+        }
 
-        const requestOptions = {
-            method: "POST",
-            headers: myHeaders,
-            body: raw,
-            redirect: "follow"
-        };
-
-        const r = await fetch("http://localhost:3000/api/add", requestOptions)
-           const result = await r.json();
-           toast(r.message)
-
-    }
-    return (
-        <div className='bg-[#E9C0E9] min-h-screen grid grid-cols-2' >
-
-            <div className="col1 flex justify-center items-center flex-col text-gray-800">
-
-                <div className='flex flex-col gap-5 my-8'>
-                    <h1 className='font-bold text-4xl'>Create your LinkUP</h1>
-                    <div className="item">
-                        <h2 className='font-medium text-xl '>Step 1: Claim Your Handle</h2>
-                        <div className="mx-4">
-                            <input value={handle || ""} onChange={e=>{sethandle(e.target.value)}} type="text" placeholder='Choose your Handle!' className='bg-white text-black px-4 py-3 my-2 mx-2 focus:outline-pink-400 rounded-full' />
-                        </div>
-                    </div>
-                    <div className="item">
-
-
-                        <h2 className='font-medium text-xl '>Step 2: Add Links</h2>
-                        <div className="mx-4">
-                            <input value={linkText || ""} onChange={e=>{setlinkText(e.target.value)}} type="text" placeholder='Enter Link Text' className='bg-white text-black px-4 py-3 mx-2 my-2 focus:outline-pink-400 rounded-full' />
-                            <input value={link || ""} onChange={e=>{setlink(e.target.value)}}type="text" placeholder='Enter Links' className='bg-white text-black px-4 py-3 mx-2 focus:outline-pink-400 rounded-full' />
-                            <button className=" py-3 px-5 mx-2 bg-pink-500 font-medium text-white rounded-4xl p-5"> Add Link </button>
-                        </div>
-                    </div>
-                    <div className="item">
-                        <h2 className='font-medium text-xl'>Step 3: Add Picture and LockIT</h2>
-                        <div className="mx-4 flex flex-col">
-                            <input value={picture || ""} onChange={e=>{setpicture(e.target.value)}} type="text" placeholder='Enter Links To Your Pictures' className='bg-white text-black px-4 py-3 mx-2 focus:outline-pink-400 rounded-full' />
-                            <button className=" py-3 px-5 mx-2 my-4 bg-pink-500 font-medium text-white rounded-4xl p-5 w-fit">Create Your LinkUP</button>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-            <div className="col2 w-full h-screen bg-[#E9C0E9]">
-                <img src="/generate.png" alt="generate ur link" className='h-full object-contain' />
-            
-            <ToastContainer />
-            </div>
-        </div>
-    )
+    })
+ })
 }
 
-export default generate
+
+  const [yourhandle, setYourhandle] = useState("");
+  const [picture, setpicture] = useState("");
 
 
+  const addLink = () => { 
+    setLinks(links.concat([{link: "", linkText: ""}]))
+    }
 
-{/* <div class="hidden relative -z-10 items-center justify-center overflow-hidden lg:!flex lg:w-[calc(100vw-48%)] bg-center bg-cover bg-no-repeat bg-cover bg-[#225AC0]" style="background-image: ;"></div> */ }
+
+  const submitLinks = async () => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+      links: links,
+      handle: yourhandle,
+      pic:picture
+    });
+console.log(raw);
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    const r = await fetch("http://localhost:3000/api/add", requestOptions);
+    const result = await r.json();
+    if(result.success){ 
+        toast.success(result.message)
+        setLinks([])
+        setpicture("")
+        setYourhandle("")
+       }
+       else{
+         toast.error(result.message)
+       }
+  };
+  return (
+    <div className="bg-[#E9C0E9] min-h-screen grid grid-cols-2">
+      <div className="col1 flex justify-center items-center flex-col text-gray-800">
+        <div className="flex flex-col gap-5 my-8">
+          <h1 className="font-bold text-4xl">Create your LinkUP</h1>
+          <div className="item">
+            <h2 className="font-medium text-xl ">Step 1: Claim Your Handle</h2>
+            <div className="mx-4">
+              <input
+                value={yourhandle || ""}
+                onChange={(e) => {
+                  setYourhandle(e.target.value);
+                }}
+                type="text"
+                placeholder="Choose your Handle!"
+                className="bg-white text-black px-4 py-3 my-2 mx-2 focus:outline-pink-400 rounded-full"
+              />
+            </div>
+          </div>
+          <div className="item">
+            <h2 className="font-medium text-xl ">Step 2: Add Links</h2>
+           {links && links.map((item,index)=>{
+            return <div key={index}className="mx-4">
+            <input
+                value={item.link || ""}
+                onChange={(e) => {
+                  handleChange(index, e.target.value, item.linkText);
+                }}
+                type="text"
+                placeholder="Enter Links"
+                className="bg-white text-black px-4 py-3 mx-2 focus:outline-pink-400 rounded-full"
+              />  
+              
+              <input
+                value={item.linkText || ""}
+                onChange={(e) => {
+                    handleChange(index, item.link, e.target.value);
+                }}
+                type="text"
+                placeholder="Enter Link Text"
+                className="bg-white text-black px-4 py-3 mx-2 my-2 focus:outline-pink-400 rounded-full"
+              />
+
+            </div>
+           })}
+
+            <button onClick={()=>addLink()} className=" py-3 px-5 mx-2 bg-pink-500 font-medium text-white rounded-4xl p-5">
+                {" "}
+               + Add Link{" "}
+              </button>
+          </div>
+          <div className="item">
+            <h2 className="font-medium text-xl">
+              Step 3: Add Picture and LockIT
+            </h2>
+            <div className="mx-4 flex flex-col">
+              <input
+                value={picture || ""}
+                onChange={(e) => {
+                  setpicture(e.target.value);
+                }}
+                type="text"
+                placeholder="Enter Links To Your Pictures"
+                className="bg-white text-black px-4 py-3 mx-2 focus:outline-pink-400 rounded-full"
+              />
+              <button disabled={picture=="" || yourhandle=="" || links[0].linkText ==""} onClick={()=>{submitLinks()}}  className="disabled:bg-slate-500 py-3 px-5 mx-2 my-4 bg-pink-500 font-medium text-white rounded-4xl p-5 w-fit">
+                Create Your LinkUP
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="col2 w-full h-screen bg-[#E9C0E9]">
+        <img
+          src="/generate.png"
+          alt="generate ur link"
+          className="h-full object-contain"
+        />
+
+        <ToastContainer />
+      </div>
+    </div>
+  );
+};
+
+export default generate;
+
